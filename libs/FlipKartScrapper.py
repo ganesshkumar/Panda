c3e01a58d6ad03bb52f3535f44de9ddbd70d5daa
root <yesyayen@gmail.com>
2014-03-01 17:08:18 +0530

    flipkart scrapper code-complete

--- libs/FlipKartScrapper.py
+++ libs/FlipKartScrapper.py
@@ -3,51 +3,64 @@ __author__ = 'anandh&sanjeev'
 from bs4 import BeautifulSoup
 
 import urllib2
+import md5
+import unicodedata
 
 class FlipKartScrapper:
 
     def __init__(self,product_pageURL,review_pageURL):
+        print product_pageURL
         self.product_page=urllib2.urlopen(product_pageURL)
         self.product_data=BeautifulSoup(self.product_page.read())
         self.review_pageURL=review_pageURL
 
-
     def get_review_count(self):
-        review_count=self.product_data.find("span",{'itemprop':'review_count'}).get_text()
-        print review_count
+        review_count=self.product_data.find("span",{'itemprop':'reviewCount'}).get_text()
         return review_count
 
     def get_Rating(self):
         RatingUserCount=self.product_data.find("span",{'itemprop':'ratingCount'}).get_text()
-        print RatingUserCount
         Rating=self.product_data.find("div",{'class':'pp-big-star'}).get_text()
-        print Rating
+        return Rating
 
 
-    def get_review_data(self):
+    def get_review_data(self,latestMD5):
+        flag = 0
         review_count=self.get_review_count();
-
-        file_review=open("Flipkart_Review.txt","a")
-        file_review_head=open("Flipkart_Review_Head.txt","a")
-
+        print review_count
+        flipkart_review = []
+        flipkart_review_header = []
         pageNum=0
-        while int(pageNum) < int(review_count)/10:
+        while int(pageNum) < int(review_count) and flag == 0:
+            print pageNum
             review_page=urllib2.urlopen(self.review_pageURL+"&start="+str(pageNum))
+
             review_data= BeautifulSoup(review_page.read())
 
             for review in review_data.findAll("p",{'class':'line bmargin10'}):
-                print review.get_text()
-                file_review.write(review.get_text())
-                print "--------------------------------------------------------------------"
+
+                newHash = unicodedata.normalize('NFKD', review.get_text().strip()).encode('ascii','ignore')
+                print md5.new(newHash).hexdigest()
+                if md5.new(newHash).hexdigest() == latestMD5:
+                    flag=1
+                    break
+                flipkart_review.append(review.get_text().strip())
+                #print "--------------------------------------------------------------------"
 
             for reviewHead in review_data.findAll("div",{'class':'line fk-font-normal bmargin5 dark-gray'}):
-                print reviewHead.get_text()
-                file_review_head.write(reviewHead.get_text())
-                print "--------------------------------------------------------------------"
-            pageNum=pageNum+10
-        file_review.close()
-        file_review_head.close()
+                if len(flipkart_review) == len(flipkart_review_header):
+                    break
+                flipkart_review_header.append(reviewHead.get_text().strip())
 
+                #print "--------------------------------------------------------------------"
+            pageNum=pageNum+10
+
+        if len(flipkart_review) != 0:
+            sendMD5=md5.new(flipkart_review[0]).hexdigest()
+        else:
+            sendMD5=latestMD5
+
+        return {'titles': flipkart_review_header, 'details': flipkart_review ,'rating': self.get_Rating(), 'md5':sendMD5}
 
 '''
 alch=AlchemyAPI()
