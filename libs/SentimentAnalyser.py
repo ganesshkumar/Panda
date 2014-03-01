bd635c03bdf9feec4e2ff6f3f63fa4e4816256a7
dnivra26 <dnivra26@gmail.com>
2014-03-02 02:22:32 +0530

    new senti

--- libs/SentimentAnalyser.py
+++ libs/SentimentAnalyser.py
@@ -1,23 +1,24 @@
 import sys
-
-#from DatumBox import DatumBox
-from alchemyapi import AlchemyAPI
-
-alchemy=AlchemyAPI()
-#datum_box=DatumBox('f78c469c6bb22ee40677b30738d69538')
+import urllib,urllib2
+import json
 
 
 class SentimentAnalyser:
 
    def __init__(self):
-       pass
+       self.url ='http://sentiment.vivekn.com/api/text/'
 
    def get_text_sentiment(self, text):
-       dict = alchemy.sentiment('text',text)
-       if 'docSentiment' in dict:
-          return dict['docSentiment']	
-       else:
-          return {'type':'invalid'}
+	values = {'txt' : text}
+	data = urllib.urlencode(values)
+	req = urllib2.Request(self.url, data)
+	response = urllib2.urlopen(req)
+	sentiment_response = response.read()
+	json_sent = json.loads(sentiment_response)
+	sentiment = json_sent['result']['sentiment']
+	print sentiment
+        return sentiment
+
 
    def get_sentiments(self,details):
        positive=0
@@ -25,31 +26,13 @@ class SentimentAnalyser:
        neutral=0
        for text in details:
           dict = self.get_text_sentiment(text)
-          if dict['type'] == 'positive':
+          if dict == 'Positive':
              positive += 1
-          elif dict['type'] == 'negative':	
+          elif dict == 'Negative':	
 	     negative += 1
           else:
              neutral += 1	
        return {'positive':positive, 'negative':negative, 'neutral':neutral}
-
-   def get_tweet_sentiment(self,tweet):
-       return datum_box.twitter_sentiment_analysis(tweet)
-        
-   def get_tweet_sentiments(self,tweets):
-       positive=0
-       negative=0 
-       neutral=0
-       for tweet in tweets:
-          result = self.get_tweet_sentiment(tweet)
-          if result == 'positive':
-             positive += 1
-          elif result == 'negative':
-             negatvie += 1
-          else:
-             neutral += 1
-       return {'positive':positive, 'negative':negative, 'neutral':neutral} 
-
 
 
 
--- test.py
+++ test.py
@@ -4,19 +4,19 @@ from AmazonScrapper import AmazonScrapper
 from SentimentAnalyser import SentimentAnalyser
 from FlipKartScrapper import FlipKartScrapper
 
-amazon = AmazonScrapper('http://www.amazon.com/SUPERSHIELDZ-Definition-Protector-Motorola-Replacements/product-reviews/B00GI3Q4OO/ref=cm_cr_dp_see_all_btm?ie=UTF8&sortBy=bySubmissionDateDescending&showViewpoints=')
+#amazon = AmazonScrapper('http://www.amazon.com/SUPERSHIELDZ-Definition-Protector-Motorola-Replacements/product-reviews/B00GI3Q4OO/ref=cm_cr_dp_see_all_btm?ie=UTF8&sortBy=bySubmissionDateDescending&showViewpoints=')
 
 flipkart = FlipKartScrapper("http://www.flipkart.com/google-nexus-5/p/itmdq9vxq6nswafg?pid=MOBDQ9VXZMHXZGBP","http://www.flipkart.com/google-nexus-5/product-reviews/ITMDQ9VXQ6NSWAFG?pid=MOBDQ9VXZMHXZGBP&sort_order=most-recent")
 
 senti = SentimentAnalyser()
 
-amazon_reviews = amazon.get_all_reviews('arvind')
+#amazon_reviews = amazon.get_all_reviews('arvind')
 flipkart_reviews = flipkart.get_review_data('arvind')
 
-amazon_analysis = senti.get_sentiments(amazon_reviews['titles'])
+#amazon_analysis = senti.get_sentiments(amazon_reviews['titles'])
 flipkart_analysis = senti.get_sentiments(flipkart_reviews['titles'])
 
-print amazon_analysis
+#print amazon_analysis
 print flipkart_analysis
 
 
