93c02343d578682bd55bff25c3f6d736d2695d64
ganesshkumar <rpganesshkumar@gmail.com>
2014-03-01 19:23:36 +0530

    Ajax call

--- index.py
+++ index.py
@@ -1,9 +1,20 @@
 import os, sys
-from flask import Flask
+from flask import Flask, render_template, url_for, request, jsonify
 
 sys.path.append('libs')
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World from arvind!'
+    return render_template("test.html", request=request)
+
+@app.route('/search', methods=['POST', 'GET'])
+def search():
+    print 'searching'
+    search_term = request.args.get('search', '', type=str)
+    print search_term
+    return jsonify(result=search_term)
+
+if __name__ == '__main__':
+    app.run()
