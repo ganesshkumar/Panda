ee3dbdd0146b74889b2c47101e25e627c7596036
ganesshkumar <rpganesshkumar@gmail.com>
2014-03-01 18:20:36 +0530

    Adding DB

---
+++ libs/MongoDBClient.py
@@ -0,0 +1,50 @@
+import os
+from pymongo import MongoClient, errors
+from datetime import datetime
+
+class MongoDBClient:
+    def __init__(self):
+        try:
+            self.client = MongoClient('mongodb://save-the-hacker:backstroke@troup.mongohq.com:10075/app22534615')
+        except errors.ConnectionFailure:
+            self.client = MongoClient('localhost',27017)
+        self.db = self.client.app22534615
+        self.reviews = self.db.reviews
+
+    @staticmethod
+    def get_empty_data():
+        dict = {
+            'title': '',
+            'title_ws': '',
+            'amzn_url': '',
+            'flip_url': '',
+            'amzn_hash': '',
+            'flip_hash': '',
+            'amzn_rating': {
+                'positive': 0,
+                'negative': 0,
+                'neutral': 0
+            },
+            'flip_rating': {
+                'positive': 0,
+                'negative': 0,
+                'neutral': 0
+            },
+            'twitter_tags': [],
+            'lastUpdated': datetime.now()
+        }
+        return dict
+
+    def insert(self, title):
+        return self.reviews.find_one({'title_ws': ''.join(e for e in title if e.isalnum()).lower()})
+
+    def save(self, data):
+        data['title_ws'] = ''.join(e for e in data['title'] if e.isalnum()).lower()
+        self.reviews.save(data)
+
+    def insert(self, data):
+        try:
+            self.reviews.insert(data)
+            return "Post added successfully"
+        except errors.DuplicateKeyError:
+            return  "Post already added"
