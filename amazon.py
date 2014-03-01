48b19046e8f60be7c093b2f6cc6efa9ef5e66876
dnivra26 <dnivra26@gmail.com>
2014-03-01 12:50:28 +0530

    amazon scrapper table changer

--- amazon.py
+++ amazon.py
@@ -3,10 +3,9 @@ from bs4 import BeautifulSoup, NavigableString
 html = urllib2.urlopen('http://www.amazon.com/Motorola-Moto-Global-Unlocked-16GB/product-reviews/B00GWR373M/?showViewpoints=1').read()
 #print html
 soup = BeautifulSoup(html)
-tables = soup.find_all('table')
-for table in tables:
-	divs=table.find_all('div',{"class":"reviewText"})
-	for div in divs:
-		print div
-		print "***********\n"
+table = soup.find('table',{"id":"productReviews"})
+divs=table.find_all('div',{"class":"reviewText"})
+for div in divs:
+	print div
+	print "***********\n"
 
