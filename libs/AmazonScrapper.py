ea43d4ccca2d4520ab326075d76e56e1ba08ca15
ganesshkumar <rpganesshkumar@gmail.com>
2014-03-01 14:05:07 +0530

    iterating

--- libs/AmazonScrapper.py
+++ libs/AmazonScrapper.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup, NavigableString
 
 class AmazonScrapper:
     def __init__(self, url_first_page):
-        self.html = urllib2.urlopen(url_first_page).read()
+        self.url = url_first_page
+        self.html = urllib2.urlopen(url_first_page+'1').read()
         self.soup = BeautifulSoup(self.html)
         self.number_of_pages = self.get_number_of_pages()
 
@@ -13,21 +14,26 @@ class AmazonScrapper:
         links = pages.find_all('a')
         return links[1].get_text()
 
-    def get_review_titles(self):
+    def get_review_titles(self, soup):
         '''get single line reviews'''
-        reviews = self.soup.find_all('span',{"style":"vertical-align:middle;"})
-        #print len(reviews)
-        #for review in reviews:
-	#    print review.find('b').get_text()
+        reviews = soup.find_all('span',{"style":"vertical-align:middle;"})
         return [x.find('b').get_text() for x in reviews]
 
-    def get_detail_review(self):
+    def get_detail_reviews(self, soup):
         '''get detailed reviews'''
-        table = self.soup.find('table',{"id":"productReviews"})
+        table = soup.find('table',{"id":"productReviews"})
         divs = table.find_all('div',{"class":"reviewText"})
         return divs
-        #print len(divs)
-        #for div in divs:
-	#    print div
-	#    print "***********\n"'''
+
+    def get_all_review_titles(self):
+        titles = []
+        for i in xrange(1, self.number_of_pages+1):
+            soup = BeautifulSoup(self.url+str(i))
+            titles.extend(self.get_review_titles(soup))
+
+    def get_all_detail_reviews(self):
+        details = []
+        for i in xrange(1, self.number_of_pages+1):
+            soup = BeautifulSoup(self.url+str(i))
+            details.extend(self.get_review_titles(soup))
 
