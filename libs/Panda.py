14c49642f3f6fc12b07271f91f52c1f87805654e
ganesshkumar <rpganesshkumar@gmail.com>
2014-03-02 10:43:50 +0530

    fixing

--- libs/Panda.py
+++ libs/Panda.py
@@ -3,6 +3,7 @@ from SentimentAnalyser import SentimentAnalyser
 from FlipKartScrapper import FlipKartScrapper
 from MongoDBClient import MongoDBClient
 
+
 class Panda:
     db_client = MongoDBClient()
 
@@ -21,12 +22,12 @@ class Panda:
         amazon_reviews = amazon.get_all_reviews(item['amzn_hash'])
         print ('Amazon reviews retrived')
         amazon_analysis = analyser.get_sentiments(amazon_reviews['titles'])
-	    print amazon_analysis
+        print amazon_analysis
         print ('Amazon Analysis Done')
         flipkart_reviews = flipkart.get_review_data('flip_hash')
         print ('Flipkart reviews retrived')
         flipkart_analysis = analyser.get_sentiments(flipkart_reviews['titles'])
-	    print flipkart_analysis
+        print flipkart_analysis
         print ('Flipkart Analysis Done')
         item['amzn_senti'] = self.add_senti(item['amzn_senti'], amazon_analysis)
         item['amzn_hash'] = amazon_reviews['md5']
